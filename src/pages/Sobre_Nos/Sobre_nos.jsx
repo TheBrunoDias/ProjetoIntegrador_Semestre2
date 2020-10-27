0d74461cb8a71fe9123c08a0cb23c64fd02656c1
Marcelo Fortes <marcelofortes306@gmail.com>
2020-10-27 20:19:51 -0300

    Página Sobre a Equipe

--- src/pages/Sobre_Nos/Sobre_nos.jsx
+++ src/pages/Sobre_Nos/Sobre_nos.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import './Sobre_nos.css';
 //IMPORTANDO IMAGENS
-import Bruno from '../../Assets/SobreEquipe/bruno.jpg';
-import Marcelo from '../../Assets/SobreEquipe/marcelo.jpg';
-import Mariana from '../../Assets/SobreEquipe/mariana.jpg';
-import Team from '../../Assets/SobreEquipe/team.png';
+import Bruno from '../../../src/Assets/img/SobreEquipe/bruno.jpg';
+import Marcelo from '../../../src/Assets/img/SobreEquipe/marcelo.jpg';
+import Mariana from '../../../src/Assets/img/SobreEquipe/mariana.jpg';
+import Team from "../../../src/Assets/img/SobreEquipe/Vector_imagem.png";
 
 export default function Sobre_nos() {
   return(
     <>
     <div className='container'>
       <div className='txt'>
-        <h1>Sobre a equipe</h1>
-        <p>A equipe é composta por alunos do 2º Período do curso de Engenharia de Software da faculdade UniAmérica.<br /><br /> O projeto vem sendo desenvolvido juntamente com acadêmicas de Pedagogia nas quais foram responsáveis por aplicar o projeto e coletar informações. Nossa equipe está responsável de digitalizar o projeto e torna-lo mais interativo e ainda mais divertido.</p>
+        <h1>Sobre a Equipe</h1>
+        <p><b>A equipe é composta por alunos do 2º Período do curso de Engenharia de Software da faculdade UniAmérica.<br /><br /> O projeto vem sendo desenvolvido juntamente com acadêmicas de Pedagogia nas quais foram responsáveis por aplicar o projeto e coletar informações. Nossa equipe está responsável de digitalizar o projeto e torna-lo mais interativo e ainda mais divertido.</b></p>
       </div>
       <div className='team'>
           <div className='team_icon'>
             <img src={Bruno} />
-            <p>Bruno Dias</p>
+            <h3>Bruno Dias</h3>
           </div>
 
           <div className='team_icon'>
             <img src={Mariana} />
-            <p>Mariana Gonzalez</p>
+            <h3>Mariana Gonzalez</h3>
           </div>
           
           <div className='team_icon'>
             <img src={Marcelo} />
-            <p>Marcelo Fortes</p>
+            <h3>Marcelo Fortes</h3>
+          </div>
+
+          <div className='team_drawn'>
+            <img src={Team} />
           </div>
       </div>
 
-      <div className='team_draw'>
-        <img src={Team} />
-      </div>
 
     </div>
     </>
