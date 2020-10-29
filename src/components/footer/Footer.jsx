c40bcf2388bdc046230b202db7ef893f2a9d2aae
Bruno Dias <brunosousadias@hotmail.com>
2020-10-29 19:42:05 -0300

    Function backToTop on Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,17 +4,26 @@ import { Link } from 'react-router-dom';
 
 import Logo from '../../../src/Assets/img/landingpage/Logo.png';
 
-export default function Footer(){
-    return(
+
+function backToTop() {
+    window.scrollBy({
+        top: -window.innerHeight*2,
+        behavior: "smooth",
+    });
+}
+
+export default function Footer() {
+
+    return (
         <footer className="footer">
             <ul className="footer-list">
-                <li>Voltar para o Topo</li>
+                <li onClick={backToTop}>Voltar para o Topo</li>
                 <li><Link to='/' className="footer-link">Inicio</Link></li>
                 <li><Link to='/Instrucoes' className="footer-link">Instruções</Link></li>
                 <li><Link to='/Sobre_nos' className="footer-link">Sobre Nós</Link></li>
                 <li><Link to='/Sobre_tea' className="footer-link">Sobre o TEA</Link></li>
             </ul>
-            <img src={Logo} alt="Uniamericon Logo"/>
+            <img src={Logo} alt="Uniamericon Logo" />
         </footer>
     );
 
