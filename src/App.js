37a52109f4cae50f182010fb68087945b24c5ca9
Bruno Dias <brunosousadias@hotmail.com>
2020-10-29 22:57:37 -0300

    Starting translate from  json file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 //COMPONENTES
@@ -11,20 +11,28 @@ import Sobre_nos from './pages/Sobre_Nos/Sobre_nos';
 import Sobre_tea from './pages/Sobre_tea/Sobre_tea';
 import Footer from './components/footer/Footer';
 
+import * as data from './server/language.json';
+
 function App() {
+
+  const [language, setLanguage] = useState(data.pt_br);
+  const [languageBool, setLinguageBool] = useState(false);
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar language={language} changeLanguage={() => {
+          setLinguageBool(!languageBool);
+          (languageBool) ? setLanguage(data.en) : setLanguage(data.pt_br)
+        }}/>
         <Switch>
-          <Route path='/' exact component={LandingPage} />
+          <Route path='/' exact render={() => <LandingPage language={language} />}/>
           <Route path='/Instrucoes' component={Instrucoes} />
           <Route path='/Sobre_nos' component={Sobre_nos} />
           <Route path='/Sobre_tea' component={Sobre_tea} />
         </Switch>
-        <Footer/>
+        <Footer />
       </Router>
-      
+
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,35 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-function Navbar() {
-  return (
-    <nav className='navbar'>
-        <div className='nav-container'>
-            <ul className='nav-menu'>
-                <li className='nav-item'>
-                    <Link to='/' className='nav-links'>
-                        Inicio
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/Instrucoes' className='nav-links'>
-                        Instruções
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/Sobre_nos' className='nav-links'>
-                        Sobre Nós
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/Sobre_tea' className='nav-links'>
-                        Sobre o TEA
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    </nav>
-  );
+
+
+function Navbar({ language, changeLanguage }) {
+
+    return (
+        <nav className='navbar'>
+            <div className='nav-container'>
+                <ul className='nav-menu'>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links'>
+                            {language.nav_bar.home}
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/Instrucoes' className='nav-links'>
+                            {language.nav_bar.instructions}
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/Sobre_nos' className='nav-links'>
+                            {language.nav_bar.about_us}
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/Sobre_tea' className='nav-links'>
+                            {language.nav_bar.about_tea}
+                        </Link>
+                    </li>
+                </ul>
+
+                <button onClick={changeLanguage}>Linguagem</button>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
