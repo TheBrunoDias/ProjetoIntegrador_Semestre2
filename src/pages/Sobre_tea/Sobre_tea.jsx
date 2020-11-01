a5074509e156a770518d5e762b294ea3cb634e08
Bruno Dias <brunosousadias@hotmail.com>
2020-11-01 10:26:11 -0300

    General corrections

--- src/pages/Sobre_tea/Sobre_tea.jsx
+++ src/pages/Sobre_tea/Sobre_tea.jsx
@@ -6,38 +6,39 @@ import Color from '../../../src/Assets/img/SobreOTea/color.png';
 import Warning from '../../../src/Assets/img/SobreOTea/warning.png';
 import Laco from "../../../src/Assets/img/SobreOTea/laço.png";
 
-export default function Sobre_tea() {
-  return(
+export default function Sobre_tea({ language }) {
+  return (
     <>
-    <div className='container_sobre_tea'>
-      <div className='txt'>
-        <h1>Transtorno do Espectro Autista</h1>
-        <p><b>O Transtorno do Espectro Autista (TEA) refere-se a uma série de condições caracterizadas por desafios com habilidades sociais, comportamentos repetitivos, fala e comunicação não-verbal, bem como por forças e diferenças únicas.<br /><br /> Os sinais mais óbvios do Transtorno do Espectro Autista tendem a aparecer entre 2 e 3 anos de idade. Em alguns casos, ele pode ser diagnosticado por volta dos 18 meses.
-</b></p>
-      </div>
-      <div className='team'>
+      <div className='container_sobre_tea'>
+        <div className='txt'>
+          <h1>{language.about_tea.title}</h1>
+          <p><b>{language.about_tea.main_text}</b></p>
+        </div>
+        <div className='team'>
           <div className='team_icon'>
             <img src={Tea} />
-            <h3>TEA</h3>
+            <h3>{language.about_tea.title_1}</h3>
+            <p>{language.about_tea.text_1}</p>
           </div>
 
           <div className='team_icon'>
             <img src={Color} />
-            <h3>COLOR</h3>
+            <h3>{language.about_tea.title_2}</h3>
+            <p>{language.about_tea.text_2}</p>
           </div>
-          
+
           <div className='team_icon'>
             <img src={Warning} />
-            <h3>WARNING</h3>
+            <p>{language.about_tea.alert}</p>
           </div>
 
           <div className='team_drawn'>
             <img src={Laco} />
           </div>
+        </div>
+
+
       </div>
-
-
-    </div>
     </>
-  ); 
+  );
 }
