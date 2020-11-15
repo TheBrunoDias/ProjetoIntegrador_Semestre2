608ea15e50c52836d9ae2e0da491e649ccd99931
Marcelo Fortes <marcelofortes306@gmail.com>
2020-11-15 16:10:48 -0300

    Testes com biblioteca Parallax

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -35,6 +35,11 @@ function Navbar() {
                             {language.nav_bar.about_tea}
                         </Link>
                     </li>
+                    <li className='nav-item'>
+                        <Link to='/NewLP' className='nav-links'>
+                            {language.nav_bar.NewLP}
+                        </Link>
+                    </li>
                 </ul>
 
                 <div className="language-menu-container">
